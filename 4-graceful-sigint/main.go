a3709d1dceaf831a703e0263f38f2fbbc447f549
Gaurav Jayaraj <gaurav.jayaraj@couchbase.com>
2023-07-20 15:46:59 +0530

    3,4,5 done

--- 4-graceful-sigint/main.go
+++ 4-graceful-sigint/main.go
@@ -13,10 +13,29 @@
 
 package main
 
+import (
+	"os"
+	"os/signal"
+)
+
 func main() {
+
 	// Create a process
 	proc := MockProcess{}
 
 	// Run the process (blocking)
-	proc.Run()
+	go proc.Run()
+
+	c := make(chan os.Signal, 1)
+	d := make(chan bool, 1)
+	signal.Notify(c, os.Interrupt)
+
+	// trap signal
+	go func() {
+		<-c
+		signal.Reset()
+		proc.Stop()
+		d <- true
+	}()
+	<-d
 }
